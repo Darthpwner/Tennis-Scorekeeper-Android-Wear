0bb0e56dd243ae500b6998ebc192591eec9489f4
Darthpwner <matthewallenlin@gmail.com>
2017-07-13 14:50:52 -0700

    Took numerical logic variables from iOS app

--- src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
+++ src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
@@ -11,8 +11,43 @@ import android.widget.TextView;
 
 public class ScoresActivity extends Activity {
 
+    // Data from previous Activities
     String match_length, ten_point_tiebreaker_format;
 
+    // Current set [1, 3]
+    int current_set = 1;
+
+    boolean is_tiebreak = false;
+
+    // Player who won the match [0, 1] where 0 is P1 and 1 is P2
+    // Used to send data back to Android app
+    int player_won = -1;
+
+    // Player serving [0, 1] where 0 is P1 and 1 is P2
+    int player_serving = 0;
+
+    // Tracks which player started serving in the tiebreak to handle the start of the next set
+    int player_serving_to_start_tiebreak = -1;
+
+    // Game score values
+    int player_1_points_won_this_game = 0;
+    int player_2_points_won_this_game = 0;
+
+    //For score announcements
+    String player_1_game_score_string = "Love";
+    String player_2_game_score_string = "Love";
+
+    // Set score values
+    int player_1_set_1_score = 0;
+    int player_2_set_1_score = 0;
+    int player_1_set_2_score = 0;
+    int player_2_set_2_score = 0;
+    int player_1_set_3_score = 0;
+    int player_2_set_3_score = 0;
+
+    int[] set_winners = {-1, -1, -1};   // Indices can be 1 or 2
+
+    // Widget connections
     // Set scores
     TextView player_1_set_1_score_text_view;
     TextView player_2_set_1_score_text_view;
