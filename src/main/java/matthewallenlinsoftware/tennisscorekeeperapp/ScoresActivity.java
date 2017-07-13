11d3edb49dd2f0b337145bc90a9f3fb32cce500c
Darthpwner <matthewallenlin@gmail.com>
2017-07-13 14:31:47 -0700

    Declared all the widgets

--- src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
+++ src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
@@ -5,10 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ScoresActivity extends Activity {
 
     String match_length, ten_point_tiebreaker_format;
+
+    // Set scores
+    TextView player_1_set_1_score_text_view;
+    TextView player_2_set_1_score_text_view;
+    TextView comma_between_set_1_and_2_text_view;
+
+    TextView player_1_set_2_score_text_view;
+    TextView set_2_dash_text_view;
+    TextView player_2_set_2_score_text_view;
+    TextView comma_between_set_2_and_3_text_view;
+
+    TextView player_1_set_3_score_text_view;
+    TextView set_3_dash_text_view;
+    TextView player_2_set_3_score_text_view;
+
+    // Game scores
+    ImageView player_1_serving_image_view;
+    TextView player_1_game_score_text_view;
+    TextView player_2_game_score_text_view;
+    ImageView player_2_serving_image_view;
+
+    // Increment scores
+    ImageButton increment_player_1_image_button;
+    TextView player_1_text_view;
+    TextView player_2_text_view;
+    ImageButton increment_player_2_image_button;
+
+    // Announcement
+    TextView announcement_text_view;
 
     private void grabTenPointTiebreakActivityData() {
         Bundle extras = getIntent().getExtras();
