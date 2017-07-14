32352ec194e1710a25ddb732df7479a87ce8fc9f
Darthpwner <matthewallenlin@gmail.com>
2017-07-13 20:09:27 -0700

    Ported most of the announcement code from iOS

--- src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
+++ src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoresActivity.java
@@ -199,9 +199,252 @@ public class ScoresActivity extends Activity {
     // onClick Actions
     public void onClickIncrementPlayerOneScore(View view) {
         System.out.println("P1");
+
+        player_1_points_won_this_game += 1;
+
+        if(!is_tiebreak) {
+            if(player_1_points_won_this_game >= 4 && player_1_points_won_this_game - player_2_points_won_this_game >= 2) {  //Game: Player 1
+                player_1_game_score_text_view.setText("0");
+                player_2_game_score_text_view.setText("0");
+
+                player_1_points_won_this_game = 0;
+                player_2_points_won_this_game = 0;
+
+                changeServer();
+
+                // Update set score
+                if(current_set == 1) {
+                    player_1_set_1_score += 1;
+                    player_1_set_1_score_text_view.setText(Integer.toString(player_1_set_1_score));
+
+                    // Player 1 won Set 1
+                    if(player_1_set_1_score == 6 && player_2_set_1_score <= 4) {    // 6-0, 6-1, 6-2, 6-3, 6-4
+
+                        current_set += 1;
+
+                        set_winners[0] = 1;
+
+                        if(match_length == "best_of_1") {   // If best of 1 set, Player 1 wins the match
+                            gameSetMatchAnnouncement("P1");
+                        } else {
+                            // Announce "Set 1: P1"
+                            setAnnouncement("P1", "1");
+                            matchScoreAnnouncement();
+                        }
+                    } else if(player_1_set_1_score == 7 && player_2_set_1_score == 5) { // 7-5
+
+                        current_set += 1;
+
+                        set_winners[0] = 1;
+
+                        if(match_length == "best_of_1") {    //If best of 1 set, Player 1 wins the match
+                            gameSetMatchAnnouncement("P1");
+                        } else {
+                            // Announce "Set 1: P1"
+                            setAnnouncement("P1", "1");
+                            matchScoreAnnouncement();
+                        }
+                    } else if(player_1_set_1_score == 6 && player_2_set_1_score == 6) { // Enter tiebreak
+                        is_tiebreak = true;
+                        player_serving_to_start_tiebreak = player_serving;
+
+                        // Announce "Game: P1"
+                        gameAnnouncement("P1");
+
+                        // Announce set score
+                        setScoreAnnouncement(current_set, player_1_set_1_score, player_2_set_1_score);
+                    } else {    //Normal Game announcement
+                        // Announce "Game: P1"
+                        gameAnnouncement("P1");
+
+                        // Announce set score
+                        setScoreAnnouncement(current_set, player_1_set_1_score, player_2_set_1_score);
+                    }
+                }
+            }
+        }
     }
 
     public void onClickIncrementPlayerTwoScore(View view) {
         System.out.println("P2");
     }
+
+    /* Alternate server each game */
+    public void changeServer() {
+        if(player_serving == 0) {   //P2 (right side) to serve
+            player_serving = 1;
+
+            player_1_serving_image_view.setVisibility(View.INVISIBLE);
+            player_2_serving_image_view.setVisibility(View.VISIBLE);
+        } else {    //P1 (left side) to serve
+            player_serving = 0;
+
+            player_1_serving_image_view.setVisibility(View.VISIBLE);
+            player_2_serving_image_view.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Calculate normal game scores (non-tiebreaker games) */
+    public void obtainGameScore() {
+        //Basic P1 scores
+        if(player_1_points_won_this_game == 0) {
+            player_1_game_score_string = "Love";
+        } else if(player_1_points_won_this_game == 1) {
+            player_1_game_score_string = "15";
+        } else if(player_1_points_won_this_game == 2) {
+            player_1_game_score_string = "30";
+        } else if(player_1_points_won_this_game == 3 || player_1_points_won_this_game == player_2_points_won_this_game) {
+            player_1_game_score_string = "40";
+        } else if(player_1_points_won_this_game - player_2_points_won_this_game == 1) {
+            player_1_game_score_string = "AD";
+        }
+
+        //Basic P2 scores
+        if(player_2_points_won_this_game == 0) {
+            player_2_game_score_string = "Love";
+        } else if(player_2_points_won_this_game == 1) {
+            player_2_game_score_string = "15";
+        } else if(player_2_points_won_this_game == 2) {
+            player_2_game_score_string = "30";
+        } else if(player_2_points_won_this_game == 3 || player_1_points_won_this_game == player_2_points_won_this_game) {
+            player_2_game_score_string = "40";
+        } else if(player_2_points_won_this_game - player_1_points_won_this_game == 1) {
+            player_2_game_score_string = "AD";
+        }
+    }
+
+    /* Score announcements */
+    // Call the game score
+    public void gameScoreAnnouncement(String server_score, String receiver_score) {
+        if(!is_tiebreak) {
+            if(server_score == "40" && receiver_score == "40") {
+//                myUtterance = AVSpeechUtterance(string: "Deuce")
+            } else if(server_score == "AD") {
+//                myUtterance = AVSpeechUtterance(string: "Ad In")
+            } else if(receiver_score == "AD") {
+//                myUtterance = AVSpeechUtterance(string: "Ad Out")
+            } else if(server_score == receiver_score) { //<Server score>-All
+//                myUtterance = AVSpeechUtterance(string: "\(server_score)-All")
+            } else { // "<Server score>-<Receiver score>"
+//                myUtterance = AVSpeechUtterance(string: "\(server_score) \(receiver_score)")
+            }
+        }  else {   //Announce tiebreak score
+            if(server_score == receiver_score) { //<Server score>-All
+//                myUtterance = AVSpeechUtterance(string: "\(server_score)-All")
+            } else { // "<Server score>-<Receiver score>"
+                if(player_serving == 0) {   //P1 serving
+//                    myUtterance = AVSpeechUtterance(string: "\(server_score) \(receiver_score), P1")
+                } else {    //P2 serving
+//                    myUtterance = AVSpeechUtterance(string: "\(server_score) \(receiver_score), P2")
+                }
+            }
+        }
+
+//        synth.speak(myUtterance)
+//
+//        preventButtonSelection()
+//        delayGameScoreAnnouncement()
+    }
+
+    public void setScoreAnnouncement(int current_set, int player_1_set_score, int player_2_set_score) {
+        if(player_1_set_score == player_2_set_score) {
+//            myUtterance = AVSpeechUtterance(string: "\(player_1_set_score)-All. Set \(current_set)")
+        } else if(player_1_set_score > player_2_set_score) {
+//            myUtterance = AVSpeechUtterance(string: "P1 leeds \(player_1_set_score) \(player_2_set_score). Set \(current_set)")
+        } else {    //P2 leads P1 in this set
+//            myUtterance = AVSpeechUtterance(string: "P2 leeds \(player_2_set_score) \(player_1_set_score). Set \(current_set)")
+        }
+
+//        synth.speak(myUtterance)
+//
+//        preventButtonSelection()
+//        delayAnnouncement()
+    }
+
+    public void matchScoreAnnouncement() {
+        if(current_set == 2) {
+            if(set_winners[0] == 1) {   // P1 won the first set
+//                myUtterance = AVSpeechUtterance(string: "P1 leeds 1 set to love")
+            } else {    // P2 won the first set
+//                myUtterance = AVSpeechUtterance(string: "P2 leeds 1 set to love")
+            }
+        } else {    //3rd set announcement
+//            myUtterance = AVSpeechUtterance(string: "1 set all")
+        }
+
+//        synth.speak(myUtterance)
+//
+//        preventButtonSelection()
+//        delayAnnouncement()
+    }
+
+    /* Umpire announcements */
+    public void gameAnnouncement(String player) {
+        announcement_text_view.setVisibility(View.VISIBLE);
+        announcement_text_view.setText("Game: " + player);
+
+//        myUtterance = AVSpeechUtterance(string: "Game: \(player)")
+//        synth.speak(myUtterance)
+//
+//        preventButtonSelection()
+//
+//        // This line could possibly be deprecated. Need to test
+//        delayAnnouncement()
+    }
+
+    public void setAnnouncement(String player, String set_number) {
+        announcement_text_view.setVisibility(View.VISIBLE);
+        announcement_text_view.setText("Set " + set_number + ": " + player);
+
+//        myUtterance = AVSpeechUtterance(string: "Game and Set \(set_number): \(player)")
+//        synth.speak(myUtterance)
+//
+//        preventButtonSelection()
+//        delayAnnouncement()
+    }
+
+    public void gameSetMatchAnnouncement(String player) {
+        announcement_text_view.setVisibility(View.VISIBLE);
+        announcement_text_view.setText("Game, Set, Match");
+
+        if(match_length == "best_of_1") {    //Best of 1 set format
+            if(player == "P1") {    //P1 wins
+//                myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_1_set_1_score) \(player_2_set_1_score)")
+            } else {    //P2 wins
+//                myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_2_set_1_score) \(player_1_set_1_score)")
+            }
+        } else {    //Best of 3 set format
+            if(player == "P1") {
+                if(set_winners[0] == set_winners[1]) {  //P1 won in straight sets
+//                    myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_1_set_1_score) \(player_2_set_1_score). \(player_1_set_2_score) \(player_2_set_2_score)")
+                } else {    //P1 won in 3 sets
+//                    myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_1_set_1_score) \(player_2_set_1_score). \(player_1_set_2_score) \(player_2_set_2_score). \(player_1_set_3_score) \(player_2_set_3_score)")
+                }
+            } else {
+                if(set_winners[0] == set_winners[1]) {  //P2 won in straight sets
+//                    myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_2_set_1_score) \(player_1_set_1_score). \(player_2_set_2_score) \(player_1_set_2_score)")
+                } else {    //P1 won in 3 sets
+//                    myUtterance = AVSpeechUtterance(string: "Game, Set, Match: \(player). \(player_2_set_1_score) \(player_1_set_1_score). \(player_2_set_2_score) \(player_1_set_2_score). \(player_2_set_3_score) \(player_1_set_3_score)")
+                }
+            }
+        }
+
+//        synth.speak(myUtterance)
+
+        // Change color for the winner
+        if(player == "P1") {
+            player_1_text_view.setTextColor(Color.GREEN);
+            player_won = 0;
+        } else {
+            player_2_text_view.setTextColor(Color.GREEN);
+            player_won = 1;
+        }
+
+        player_1_serving_image_view.setVisibility(View.INVISIBLE);
+        player_2_serving_image_view.setVisibility(View.INVISIBLE);
+
+//        preventButtonSelection()
+//        delayGameSetMatch()
+    }
 }
+
